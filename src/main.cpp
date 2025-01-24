d16f0d38d98d725a469a0a9ba2d1526061fe6940
Ahmed Madeh Noah <ahmed.madeh.noah@gmail.com>
2025-01-24 04:31:19 +0200

    Add `sign_out`

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,11 @@ int main() {
         int pin = 0;
         if (login(username, pin)) {
             printf("Welcome %s\n", username.c_str());
-            const int operation = show_main_menu();
+            while (true) {
+                const int operation = show_main_menu();
+                if (operation == 0)
+                    break;
+            }
         }
     }
     return 0;
