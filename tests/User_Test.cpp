7f28ffb047669f595771e0ab058d928c45ade25c
Ahmed Madeh Noah <ahmed.madeh.noah@gmail.com>
2025-01-30 16:06:13 +0200

    Change `TEST(UsersTest` to `TEST(UserTest`

--- tests/User_Test.cpp
+++ tests/User_Test.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include "User.h"
 
-TEST(UsersTest, SubtractTest) {
+TEST(UserTest, SubtractTest) {
     EXPECT_EQ(subtract(5, 3), 2);
     EXPECT_EQ(subtract(10, 15), -5);
     EXPECT_EQ(subtract(0, 0), 0);
